1746b4baeb3a4fcec66336a00e6e7c6df89fda2f
Flammemus <83021665+Flammemus@users.noreply.github.com>
2023-12-06 10:21:00 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div class="main">
+    <div class="characterMain">
+      <span class="characterHeader">CHARACTER</span>
+      <div class="characterContainer">
+        <div class="characterBackground" id="characterBackgroundTop"></div>
+        <div class="characterStatsContainer">
+          <div class="characterLevelContainer">
+            <div class="characterNameContainer">
+
+            </div>
+            <div class="characterLevelContainer2">
+              
+            </div>
+          </div>
+          <div class="characterAttributesContainer">
+
+          </div>
+        </div>
+        <div class="characterBackground" id="characterBackgroundBottom"></div>
+      </div>
+    </div>
+    <div class="skillsMain">
+
+    </div>
+  </div>
+</main>
